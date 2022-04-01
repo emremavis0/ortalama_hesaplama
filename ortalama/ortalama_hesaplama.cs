931ad1df9b1b2097de91fdeca1b6a84a3fe30f75
Emre Mavis <102695699+emremavis0@users.noreply.github.com>
2022-04-01 10:01:45 +0300

    ortalama
    c# proje

---
+++ ortalama/ortalama_hesaplama.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ortalama_hesaplama
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sinav1, sinav2, ortalama;
+            Console.WriteLine("1.sınav Notunu Giriniz.");
+            sinav1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("2.sınav Notunu Giriniz.");
+            sinav2 = Convert.ToDouble(Console.ReadLine());
+            ortalama = (sinav1 * 50/100) + (sinav2 * 50/100);
+
+           if (ortalama<=100)
+            {
+                Console.WriteLine("Ortalamanız:"+ ortalama);
+            } 
+        }
+    }
+
+
+
+
+
+
+
+}
